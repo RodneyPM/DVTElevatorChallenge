89cad05b894653f75665556812cb47ba910458a4
Rodney <prideemrm@yahoo.co.za>
2024-06-05 15:23:09 +0200

    add elevator controls methods and data

---
+++ Application/Services/ElevatorService.cs
@@ -0,0 +1,70 @@
+﻿using ElevatorChallenge.Application.DTOs;
+using ElevatorChallenge.Domain.Entities;
+using ElevatorChallenge.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorChallenge.Application.Services
+{
+    public class ElevatorService
+    {
+        private readonly IElevatorRepository _elevatorRepository;
+
+        public ElevatorService(IElevatorRepository elevatorRepository)
+        {
+            _elevatorRepository = elevatorRepository;
+        }
+
+        public async Task RequestElevatorAsync(int floor, List<PassengerDTO> passengerDTOs)
+        {
+            var elevator = _elevatorRepository.GetNearestAvailableElevator(floor);
+            if (elevator != null)
+            {
+                elevator.RequestFloor(floor);
+                _elevatorRepository.Save(elevator);
+
+                await OperateElevatorAsync(elevator, floor, passengerDTOs);
+            }
+            else
+            {
+                Console.WriteLine("No available elevators at the moment.");
+            }
+        }
+
+        public async Task OperateElevatorAsync(Elevator elevator, int requestFloor, List<PassengerDTO> passengerDTOs)
+        {
+            var nextFloor = elevator.GetNextFloorRequest();
+            while (nextFloor.HasValue)
+            {
+                await elevator.MoveToFloorAsync(nextFloor.Value);
+
+                // Add passengers when the elevator arrives at the requested floor
+                if (elevator.CurrentFloor == requestFloor)
+                {
+                    var passengers = passengerDTOs
+                        .Select(dto => new Passenger(dto.DestinationFloor))
+                        .ToList();
+
+                    elevator.AddPassengers(passengers);
+                }
+
+                // Remove passengers who reached their destination
+                elevator.RemovePassengers();
+
+                nextFloor = elevator.GetNextFloorRequest();
+                _elevatorRepository.Save(elevator);
+            }
+        }
+
+        public void DisplayElevatorStatuses()
+        {
+            foreach (var elevator in _elevatorRepository.GetAllElevators())
+            {
+                elevator.DisplayStatus();
+            }
+        }
+    }
+}
